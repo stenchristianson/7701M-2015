b4af043e71c6ad72cd7d1c0dc427aa16eb9d3987
Alex van de Sandt <alexvds17@gmail.com>
2015-10-26 17:42:32 -0400

    Added driveTime function

--- MainDriveFunctions.h
+++ MainDriveFunctions.h
@@ -20,3 +20,16 @@ void tank4 () {
 
 	setDriveSpeeds (left, right, left, right);
 }
+
+void driveTime (int left = 0, int right = 0, int time = 0) {
+	if      (left >= 1 && right >= 1) setDriveSpeeds (127, 127, 127, 127, time);
+	else if (left >= 1 && right == 0) setDriveSpeeds (127, 0, 127, 0, time);
+	else if (left >= 1 && right <= 1) setDriveSpeeds (127, -127, 127, -127, time);
+	else if (left == 0 && right >= 1) setDriveSpeeds (0, 127, 0, 127, time);
+	else if (left == 0 && right == 0) setDriveSpeeds (0, 0, 0, 0, time);
+	else if (left == 0 && right <= 0) setDriveSpeeds (0, -127, 0, -127, time);
+	else if (left <= 1 && right >= 1) setDriveSpeeds (-127, 127, -127, 127, time);
+	else if (left <= 1 && right == 0) setDriveSpeeds (-127, 0, -127, 0, time);
+	else if (left <= 1 && right <= 1) setDriveSpeeds (-127, -127, -127, -127, time);
+	else writeDebugStreamLine ("Error: driveTime(int left, int right, int time): Invalid Parameters");
+}
