c318521fffc606a85adeb6a9f7d99fcc3c923f0e
Alex van de Sandt <alexvds17@gmail.com>
2015-10-26 18:13:05 -0400

    Added basic precise distance function

--- MainDriveFunctions.h
+++ MainDriveFunctions.h
@@ -33,3 +33,60 @@ void driveTime (int left = 0, int right = 0, int time = 0) {
 	else if (left <= 1 && right <= 1) setDriveSpeeds (-127, -127, -127, -127, time);
 	else writeDebugStreamLine ("Error: driveTime(int left, int right, int time): Invalid Parameters");
 }
+
+void driveDistanceTick (int leftTick, int rightTick, int tol = 50, float speed = 1) {
+	nMotorEncoder[driveLeftRear] = 0;
+	nMotorEncoder[driveRightRear] = 0;
+
+	bool leftOnTarget;
+	bool rightOnTarget;
+
+	while (!leftOnTarget && !rightOnTarget) {
+		//Initialize motor encoder
+		int leftDist = nMotorEncoder[driveLeftFront];
+		int rightDist = nMotorEncoder[driveRightFront];
+
+		//Variables to expedite setting the motor speeds
+		int leftSpeed;
+		int rightSpeed;
+
+		while (!leftOnTarget && !rightOnTarget) {
+			//'Alias' variables
+			leftSpeed = nMotorEncoder[driveLeftFront];
+			rightSpeed = nMotorEncoder[driveRightFront];
+
+			//Check left side
+			if (leftDist - tol > leftTick) {
+				leftSpeed = -127;
+				leftOnTarget = false;
+			}
+			else if (leftDist + tol < leftTick) {
+				leftSpeed = 127;
+				leftOnTarget = false;
+			}
+			else {
+				leftSpeed = 0;
+				leftOnTarget = true;
+			}
+
+			//Check right side
+			if (rightDist - tol > rightTick) {
+				rightSpeed = -127;
+				rightOnTarget = false;
+			}
+			else if (rightDist + tol < rightTick) {
+				rightSpeed = 127;
+				rightOnTarget = false;
+			}
+			else {
+				rightSpeed = 0;
+				rightOnTarget = true;
+			}
+
+			//Set the drive speeds
+			setDriveSpeeds (leftSpeed, rightSpeed, leftSpeed, rightSpeed);
+
+			//Loop breaks when they both reach the target
+		}
+	}
+}
