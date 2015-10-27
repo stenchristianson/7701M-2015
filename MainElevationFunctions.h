a3c3b5a8de90a83ec0153ad91cab102662492736
Alex van de Sandt <alexvds17@gmail.com>
2015-10-27 10:22:09 -0400

    Added setElevationSpeeds function

--- MainElevationFunctions.h
+++ MainElevationFunctions.h
@@ -1,1 +1,11 @@
+void setElevationSpeeds (int leftSpeed, int rightSpeed, int time = 0) {
+	motor[elevLeft] = leftSpeed;
+	motor[elevRight] = rightSpeed;
 
+	if (time > 0) {
+		wait1Msec (time);
+
+		motor[elevLeft] = 0;
+		motor[elevRight] = 0;
+	}
+}
