ca229df2bd03c65d170e30db2bab1657c5589562
Alex van de Sandt <alexvds17@gmail.com>
2015-10-28 09:52:32 -0400

    Added elevLiftMax and elevLiftMin

--- MainElevationFunctions.h
+++ MainElevationFunctions.h
@@ -26,11 +26,11 @@ void elevPot (int leftPot, int rightPot, float speed = 1) {
 	int rightCurr = SensorValue[elevRight];
 
 	while (!leftOnTarget && !rightOnTarget) {
-		if ((leftCurr < leftPot && leftCurr < MAX_ELEV_LEFT) || leftCurr < MIN_ELEV_LEFT) {
+		if ((leftCurr < leftPot && leftCurr < MAX_ELEV_LEFT) || leftCurr < MIN_ELEV_LEFT - 100) {
 			leftSpeed = 127 * speed;
 			leftOnTarget = false;
 		}
-		else if ((leftCurr > leftPot && leftCurr > MIN_ELEV_LEFT) || leftCurr > MAX_ELEV_LEFT) {
+		else if ((leftCurr > leftPot && leftCurr > MIN_ELEV_LEFT) || leftCurr > MAX_ELEV_LEFT + 100) {
 			leftSpeed = -127 * speed;
 			leftOnTarget = false;
 		}
@@ -39,11 +39,11 @@ void elevPot (int leftPot, int rightPot, float speed = 1) {
 			leftOnTarget = true;
 		}
 
-		if ((rightCurr < rightPot && rightCurr < MAX_ELEV_RIGHT) || rightCurr < MIN_ELEV_RIGHT) {
+		if ((rightCurr < rightPot && rightCurr < MAX_ELEV_RIGHT) || rightCurr < MIN_ELEV_RIGHT - 100) {
 			rightSpeed = 127 * speed;
 			rightOnTarget = false;
 		}
-		else if ((rightCurr > rightPot && rightCurr > MIN_ELEV_RIGHT) || rightCurr > MAX_ELEV_RIGHT) {
+		else if ((rightCurr > rightPot && rightCurr > MIN_ELEV_RIGHT) || rightCurr > MAX_ELEV_RIGHT + 100) {
 			rightSpeed = -127 * speed;
 			rightOnTarget = false;
 		}
@@ -54,4 +54,12 @@ void elevPot (int leftPot, int rightPot, float speed = 1) {
 
 		setElevSpeeds (leftSpeed, rightSpeed);
 	}
+}
+
+void elevLiftMax (float speed = 1) {
+	elevPot (MAX_ELEV_LEFT, MAX_ELEV_RIGHT, speed);
+}
+
+void elevLiftMin (float speed = 1) {
+	elevPot (MIN_ELEV_LEFT, MIN_ELEV_RIGHT, speed);
 }
