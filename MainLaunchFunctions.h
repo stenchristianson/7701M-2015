560690fa1dc44688cf3a38508c63e321383190f6
Alex van de Sandt <alexvds17@gmail.com>
2015-10-28 14:49:31 -0400

    Added setConveyorSpeeds function

--- MainLaunchFunctions.h
+++ MainLaunchFunctions.h
@@ -1,1 +1,10 @@
+void setConveyorSpeeds (signed int speed, int time = 0) {
+	motor[convLeft] = speed;
+	motor[convRight] = speed;
 
+	if (time > 0) {
+		wait1Msec(time);
+		motor[convLeft] = 0;
+		motor[convRight] = 0;
+	}
+}
