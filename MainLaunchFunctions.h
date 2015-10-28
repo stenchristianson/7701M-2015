8494a17ac3482cb865a7d7effae4db54df440f3e
Alex van de Sandt <alexvds17@gmail.com>
2015-10-28 15:12:44 -0400

    Added setLaunchSpeeds function to set the speeds of the flywheels

--- MainLaunchFunctions.h
+++ MainLaunchFunctions.h
@@ -1,3 +1,5 @@
+#define FLYWHEEL_ACCELERATION 10
+
 void setConveyorSpeeds (signed int speed, int time = 0) {
 	motor[convLeft] = speed;
 	motor[convRight] = speed;
@@ -6,5 +8,16 @@ void setConveyorSpeeds (signed int speed, int time = 0) {
 		wait1Msec(time);
 		motor[convLeft] = 0;
 		motor[convRight] = 0;
+	}
+}
+
+void setLaunchSpeeds (signed int speed, int time = 0) {
+	motor[flyLeft] = speed;
+	motor[flyRight] = speed;
+
+	if (time > 0) {
+		wait1Msec(time);
+		motor[flyLeft] = 0;
+		motor[flyRight] = 0;
 	}
 }
