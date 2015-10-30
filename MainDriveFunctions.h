8d54d6a51c21c511cec9c41f3dfe087abbf6556f
Alex van de Sandt <alexvds17@gmail.com>
2015-10-29 22:31:22 -0400

    Added deadband to drive motors in setDriveSpeeds

--- MainDriveFunctions.h
+++ MainDriveFunctions.h
@@ -4,10 +4,14 @@
 #define TICKS_PER_DEG  10
 
 void setDriveSpeeds (int lf, int rf, int lr, int rr, unsigned int time = 0) {
-	motor[driveLeftFront] = lf;
-	motor[driveLeftRear] = lr;
-	motor[driveRightFront] = rf;
-	motor[driveRightRear] = rr;
+	if (abs(lf) > MOTOR_DEADBAND) motor[driveLeftFront] = lf;
+	else motor[driveLeftFront] = 0;
+	if (abs(rf) > MOTOR_DEADBAND) motor[driveRightFront] = rf;
+	else motor[driveRightFront] = 0;
+	if (abs(lr) > MOTOR_DEADBAND) motor[driveLeftRear] = lr;
+	else motor[driveLeftRear] = 0;
+	if (abs(rr) > MOTOR_DEADBAND) motor[driveRightRear] = rr;
+	else motor[driveRightRear] = 0;
 
 	if (time > 0) {
 		wait1Msec (time);
