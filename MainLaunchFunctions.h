111d057ac4ae5725754471b67f1d09efee5e117e
Alex van de Sandt <alexvds17@gmail.com>
2015-10-29 22:00:00 -0400

    Wrote the accelFlyWheel task, should work at this point

--- MainLaunchFunctions.h
+++ MainLaunchFunctions.h
@@ -26,10 +26,33 @@ void setLaunchSpeeds (signed int speed, int time = 0) {
 #define MOTOR_MIN_VALUE (-127)
 #define FLY_ACCEL 10//In [motor units]/s/s
 
+//Support function for accelFlyWheel, rounds a float to the nearest whole number
+int round (float f) {
+	return (f > 0) ? (int)(f + 0.5) : (int) (f - 0.5);
+}
+
 task accelFlyWheel () {
+	int currLeft = motor[flyLeft];
+	int currRight = motor[flyRight];
+
+	int period = round (1000 / FLY_ACCEL);
+
+	while (currLeft != flySpeed || currRight != flySpeed) {
+		// Check left
+		if (currLeft > flySpeed && currLeft > MOTOR_MIN_VALUE) currLeft--;
+		else if (currLeft < flySpeed && currLeft < MOTOR_MAX_VALUE) currLeft++;
+
+		//Check right
+		if (currRight > flySpeed && currRight > MOTOR_MIN_VALUE) currRight--;
+		else if (currRight < flySpeed && currRight < MOTOR_MAX_VALUE) currRight++;
+
+		wait1Msec(period);
+
+		if (period == 0) EndTimeSlice();
+	}
 
 }
 
-void manageAccelFlyWheelTask () {
+void changeFlySpeed (int speed) {
 
 }
