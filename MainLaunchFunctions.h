b4b26185087c8b030148fb86261f0034d821474f
Alex van de Sandt <alexvds17@gmail.com>
2015-10-29 22:35:15 -0400

    Added a deadband to setLaunchSpeeds and setConveyorSpeeds

--- MainLaunchFunctions.h
+++ MainLaunchFunctions.h
@@ -1,8 +1,14 @@
 #define FLYWHEEL_ACCELERATION 10
 
 void setConveyorSpeeds (signed int speed, int time = 0) {
-	motor[convLeft] = speed;
-	motor[convRight] = speed;
+	if (abs(speed) > MOTOR_DEADBAND) {
+		motor[convLeft] = speed;
+		motor[convRight] = speed;
+	}
+	else {
+		motor[convLeft] = 0;
+		motor[convRight] = 0;
+	}
 
 	if (time > 0) {
 		wait1Msec(time);
@@ -14,6 +20,15 @@ void setConveyorSpeeds (signed int speed, int time = 0) {
 void setLaunchSpeeds (signed int speed, int time = 0) {
 	motor[flyLeft] = speed;
 	motor[flyRight] = speed;
+
+	if (abs(speed) > MOTOR_DEADBAND) {
+		motor[flyLeft] = speed;
+		motor[flyRight] = speed;
+	}
+	else {
+		motor[flyLeft] = 0;
+		motor[flyRight] = 0;
+	}
 
 	if (time > 0) {
 		wait1Msec(time);
