7a9c27885cf1ca05e0ad8725e7ec9ee513b9b5e1
Alex van de Sandt <alexvds17@gmail.com>
2015-10-30 23:56:17 -0400

    Imported elevation control

--- Drive Control.c
+++ Drive Control.c
@@ -24,3 +24,31 @@ void driveTank () {
 
 	setDriveSpeeds (left, right, left, right);
 }
+
+void setElevSpeeds (int leftSpeed, int rightSpeed, int time = 0) {
+	if (abs(leftSpeed) > MOTOR_DEADBAND) motor[elevLeft] = leftSpeed;
+	else motor[elevLeft] = 0;
+
+	if (abs(rightSpeed) > MOTOR_DEADBAND) motor[elevRight] = rightSpeed;
+	else motor[elevRight] = 0;
+
+	motor[elevLeft] = leftSpeed;
+	motor[elevRight] = rightSpeed;
+
+	if (time > 0) {
+		wait1Msec (time);
+
+		motor[elevLeft] = 0;
+		motor[elevRight] = 0;
+	}
+}
+
+void elevJoystickControl () {
+	if (vexRT[Btn8U]) {
+		setElevSpeeds (127, 127);
+	}
+	else if (vexRT[Btn8D]) {
+		setElevSpeeds (-127, -127);
+	}
+	else setElevSpeeds (0,0);
+}
